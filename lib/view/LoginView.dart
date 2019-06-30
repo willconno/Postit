4622daa6291e1eb315491aa84e4948330a1ec757
William Connelly <william@clientvault.com.au>
2019-06-30 16:39:57 +0800

    - Added firebase auth and google sign in

--- lib/view/LoginView.dart
+++ lib/view/LoginView.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginWidget extends StatefulWidget {
   @override
@@ -35,12 +37,29 @@ class LoginState extends State<LoginWidget> {
                       ),
                       elevation: 3,
                       onPressed: () {
-                        Navigator.of(context).pushReplacementNamed("/notes");
+                        if (_signIn() != null) {
+                          Navigator.of(context).pushReplacementNamed("/notes");
+                        }
                       },
                     )
                   ],
                 )
               ],
             ))));
+  }
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googleAuth = GoogleSignIn();
+
+  Future<FirebaseUser> _signIn() async {
+    GoogleSignInAccount account = await _googleAuth.signIn();
+    GoogleSignInAuthentication auth = await account.authentication;
+    print(auth);
+
+    var provider = GoogleAuthProvider.getCredential(idToken: auth.idToken, accessToken: auth.accessToken);
+
+    FirebaseUser user = await _firebaseAuth.signInWithCredential(provider);
+    print(user);
+    return user;
   }
 }
